e314c5d15dc7a35d3726a97e4a43ba095f21fcf4
Your Name <you@example.com>
2023-08-14 12:17:56 +0300

    Fix build script

--- build_all.sh
+++ build_all.sh
@@ -2,5 +2,6 @@
 
 set -e
 
+docker buildx create --use
 docker login
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -f Multi-arch.dockerfile -t shuricksumy/openvpn-server:latest . --push --no-cache
