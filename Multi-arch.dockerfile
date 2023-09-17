67cd84baf8900798797078a1576693d7439a4a8f
shuricksumy <suchkov.av@gmail.com>
2023-09-18 01:54:14 +0300

    Update docker

--- Multi-arch.dockerfile
+++ Multi-arch.dockerfile
@@ -7,7 +7,7 @@ RUN apt update && apt install -y iproute2 curl
 ADD scripts/openvpn-install-v2.sh /opt/openvpn-install-v2.sh
 ADD scripts/setup.sh /opt/setup.sh
 ADD scripts/install_pkg.sh /opt/install_pkg.sh
-ADD scripts/EasyRSA-3.1.5.tgz ~/EasyRSA-3.1.5.tgz
+ADD scripts/EasyRSA-3.1.6.tgz ~/EasyRSA-3.1.6.tgz
 
 RUN /opt/install_pkg.sh
 
--- build_all.sh
+++ build_all.sh
@@ -4,4 +4,4 @@ set -e
 
 docker buildx create --use
 docker login
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -f Multi-arch.dockerfile -t shuricksumy/openvpn-server:latest . --push --no-cache
+docker buildx build --platform linux/amd64,linux/arm64 -f Multi-arch.dockerfile -t shuricksumy/openvpn-server:latest . --push --no-cache
